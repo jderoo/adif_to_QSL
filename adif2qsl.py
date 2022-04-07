d4c76d70f836d7be1ed9ae4e7991810115c5dc6e
mbridak <michael.bridak@gmail.com>
2022-04-06 19:17:14 -0700

    Made it better...

--- adif2qsl.py
+++ adif2qsl.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+Takes an adif log 'log.adi' from the local directory,
+looks up the callsign on QRZ. Gets the email address.
+Fills out the card with adif info.
+Emails card to contact.
+"""
+
+# Who's at fault_____: Mike K6GTE
+# Where to yell at me: michael.bridak@gmail.com
 
 import smtplib
 from os.path import basename
@@ -10,13 +19,17 @@ from PIL import Image, ImageDraw, ImageFont
 import adif_io
 from lookup import QRZlookup
 
-my_email_address = "mbridak@sergio"  # your from email address
+my_email_address = "youremail@address.com"
 
-mailserver = "smtp.gmail.com"
+mailserver = "smtp.gmail.com"  # your mail server
 
-email_text = "Thanks for the QSO, 73, Mike K6GTE."  # message body
+username = my_email_address  # may or maynot be same as email addr.
 
-qrz = QRZlookup("AC4LL", "Secret")  # enter your userid and password
+password = "secretpassword"
+
+email_text = "Thanks for the QSO, 73, Mike AC4LL."  # message body
+
+qrz = QRZlookup("AC4LL", "secretpassword")  # enter your userid and password
 
 log = adif_io.read_from_file("./log.adi")
 
@@ -36,14 +49,15 @@ def send_mail(send_from, send_to, subject, text, files=None, server="127.0.0.1")
     for f in files or []:
         with open(f, "rb") as fil:
             part = MIMEApplication(fil.read(), Name=basename(f))
-        # After the file is closed
         part["Content-Disposition"] = 'attachment; filename="%s"' % basename(f)
         msg.attach(part)
 
     try:
-        smtp = smtplib.SMTP(server, 587)
-        smtp.sendmail(send_from, send_to, msg.as_string())
-        smtp.close()
+        server = smtplib.SMTP_SSL(server, 465)
+        server.ehlo()
+        server.login(username, password)
+        server.sendmail(send_from, send_to, msg.as_string())
+        server.close()
     except smtplib.SMTPException as err:
         print(f"Whoops! SMTP error - {err}")
 
@@ -51,6 +65,9 @@ def send_mail(send_from, send_to, subject, text, files=None, server="127.0.0.1")
 for record in log[0]:
     grid, name, nickname, email, error_text = qrz.lookup(record["CALL"])
     time_utc = f"{record['TIME_ON'][0:2]}:{record['TIME_ON'][2:4]}"
+
+    # If there is a FREQ tag in the adif record it will favor that.
+    # otherwise use whats in the BAND field.
     freq_band = None
     if "FREQ" in record:
         freq_band = "{:.3f}".format(float(record["FREQ"]))
@@ -61,14 +78,20 @@ for record in log[0]:
         f"{record['QSO_DATE'][0:4]}-{record['QSO_DATE'][4:6]}-{record['QSO_DATE'][6:8]}"
     )
     print(
-        f"{record['CALL']} {record['MODE']} {freq_band} {record['BAND']} {time_utc} {record['RST_SENT']} {record['RST_RCVD']} {email} {error_text}"
+        f"{record['CALL']} {record['MODE']} {freq_band} {record['BAND']} "
+        f"{time_utc} {record['RST_SENT']} {record['RST_RCVD']} {email}"
     )
-    print(record)
 
     img = Image.open("qslcard.png")
     working_image = ImageDraw.Draw(img)
     Font = ImageFont.truetype("./JetBrainsMono-Regular.ttf", 24)
     smFont = ImageFont.truetype("./JetBrainsMono-Regular.ttf", 14)
+    #
+    # This section places the text on the card.
+    # The first group  of numbers is the offset in pixels from upper left 0,0
+    # Normal text anchor point is upper left of text.
+    # lines with anchor='ms' places the anchor point in the middle of the text.
+    #
     working_image.text(
         (55, 331), record["CALL"], font=Font, anchor="ms", fill=(0, 0, 0)
     )
@@ -85,15 +108,16 @@ for record in log[0]:
         (430, 320), " RCVD: " + record["RST_RCVD"], font=smFont, fill=(0, 0, 0)
     )
     img.save("QSL_CARD.png")
-    send_mail(
-        my_email_address,
-        [
-            email,
-        ],
-        f"Our QSO on {the_date}",
-        email_text,
-        [
-            "QSL_CARD.png",
-        ],
-        mailserver,
-    )
+    if email:
+        send_mail(
+            my_email_address,
+            [
+                email,
+            ],
+            f"Our QSO on {the_date}",
+            email_text,
+            [
+                "QSL_CARD.png",
+            ],
+            mailserver,
+        )
